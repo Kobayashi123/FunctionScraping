4904fffe851d045c51d52914d7247e5a98ea282d
Kobayashi <shun123k@gmail.com>
2022-03-10 09:35:25 +0900

    URL先のファイルを出力する機能の実装

--- built_in_rust_function_by_rust/src/main.rs
+++ built_in_rust_function_by_rust/src/main.rs
@@ -1,12 +1,17 @@
-//use reqwest;
+use reqwest;
 //use scraper::{Html, Selector};
 use std::fs::File;
 //use std::io::{self, prelude::*, Write};
 use std::io::Write;
 
 fn main() {
+    let body = reqwest::blocking::get("https://doc.rust-lang.org/std/index.html")
+        .unwrap()
+        .text()
+        .unwrap();
+
     let mut file = File::create("RustBuiltInFunction.html").unwrap();
-    let str = String::from("temporary-string");
+    let str = String::from(body);
 
     write!(file, "{}", str).unwrap();
     file.flush().unwrap();
