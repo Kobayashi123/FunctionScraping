c95e6d2967c4ad78528541f07107263f7c5cd16c
Kobayashi <shun123k@gmail.com>
2022-03-11 07:04:59 +0900

    URL先から関数名とリンクを取得する機能の実装

--- built_in_rust_function_by_rust/src/main.rs
+++ built_in_rust_function_by_rust/src/main.rs
@@ -1,18 +1,38 @@
 use reqwest;
-//use scraper::{Html, Selector};
+use scraper::{Html, Selector};
 use std::fs::File;
-//use std::io::{self, prelude::*, Write};
 use std::io::Write;
 
 fn main() {
+    let selector = Selector::parse("div.item-left > a.primitive").unwrap();
+
     let body = reqwest::blocking::get("https://doc.rust-lang.org/std/index.html")
         .unwrap()
         .text()
         .unwrap();
 
-    let mut file = File::create("RustBuiltInFunction.html").unwrap();
-    let str = String::from(body);
+    let document = Html::parse_document(&body);
 
-    write!(file, "{}", str).unwrap();
+    let elements = document.select(&selector);
+
+    let mut function_url;
+    let mut function_name;
+
+    let mut output_to_html = String::new();
+    for element in elements {
+        function_name = element.text().next().unwrap();
+
+        function_url = format!(
+            "{}{}",
+            "https://doc.rust-lang.org/std/",
+            element.value().attr("href").unwrap()
+        );
+
+        output_to_html.push_str(&format!("{} : {}\n", function_name, function_url));
+        println!("{} : {}", function_name, function_url);
+    }
+
+    let mut file = File::create("RustBuiltInFunction.html").unwrap();
+    write!(file, "{}", output_to_html).unwrap();
     file.flush().unwrap();
 }
